872f5a1db62127fb89e17d5981f8632fc99a2d8e
Josh Higginson <joshuahigginson1@gmail.com>
2020-07-23 14:46:48 +0100

    Uploading examples for templates and routes in Flask to github.

---
+++ flask-app/app.py
@@ -0,0 +1,31 @@
+"""
+Task: Making a Flask blog, using the QA tutorial.
+
+The purpose of this assignment is to introduce us to Flask and Jinja
+
+Dev Notes:
+
+"""
+
+# Imports --------------------------------------------------------------------------------
+
+from application import app  # imports app object from ./application/__init__.py
+
+# Classes --------------------------------------------------------------------------------
+
+
+# Functions ------------------------------------------------------------------------------
+
+
+# Methods --------------------------------------------------------------------------------
+
+
+# Define Variables -----------------------------------------------------------------------
+
+
+# Execute Code ---------------------------------------------------------------------------
+
+# if the file is being run directly, and not imported, then start the application.
+
+if __name__ == '__main__':
+ app.run(debug=True, host='0.0.0.0')
---
+++ flask-app/application/__init__.py
@@ -0,0 +1,21 @@
+"""
+Task:
+
+
+Dev Notes:
+
+
+"""
+
+# Imports --------------------------------------------------------------------------------
+
+# import Flask class from the flask module
+from flask import Flask
+
+# create a new instance of Flask, and store it in variable app
+app = Flask(__name__)
+
+# import the ./application/routes.py file
+from application import routes
+
+# Define Variables -----------------------------------------------------------------------
